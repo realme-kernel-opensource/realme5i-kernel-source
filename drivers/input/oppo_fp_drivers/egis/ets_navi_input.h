97912b2a27204a9151aa00660d90075449bc2861
realme-kernel-opensource <realme-kernel-opensource@realme.com>
2020-04-14 15:49:06 +0800

    Upload realme5i-kernel-source

---
+++ drivers/input/oppo_fp_drivers/egis/ets_navi_input.h
@@ -0,0 +1,5 @@
+
+extern void uinput_egis_init(struct etspi_data *egistec);
+extern void uinput_egis_destroy(struct etspi_data *egistec);
+extern void sysfs_egis_init(struct etspi_data *egistec);
+extern void sysfs_egis_destroy(struct etspi_data *egistec);
